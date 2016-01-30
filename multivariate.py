cedd377e4ab7580ba41bc9a4ea50d79dcdec414f
Mike McClendon <mike.mcclendon@gmail.com>
2016-01-30 18:22:30 -0500

    Second

--- multivariate.py
+++ multivariate.py
@@ -4,25 +4,36 @@ import numpy as np
 import statsmodels.api as sm
 
 loansData = pd.read_csv('LoanStats3a.csv', skiprows=1)
+loansData2 = loansData[['annual_inc', 'int_rate', 'home_ownership']]
+ld = loansData2.dropna(subset = ['annual_inc', 'int_rate', 'home_ownership'])
+#creating subset, dropping na
 
-loansData['hodummy'] = map(lambda x: 0 if x == 'RENT' else 1, loansData['home_ownership'])
-loansData['cleanintrate'] = map(lambda x: str(x)[:-1], loansData['int_rate'])
-loansData['cleanintratenum'] = loansData[['cleanintrate']].convert_objects(convert_numeric=True) #says is deprecated, but to_numeric doesn't work
-cleanintrate = map(lambda x: x/100, loansData['cleanintratenum'])
-loansData['int_rate'] = cleanintrate
-ld = loansData.dropna(subset = ['annual_inc', 'int_rate', 'hodummy'])
-#cleaning, dropping nulls, and converting data to dummies
+ld['hodummy'] = map(lambda x: 1 if x == 'OWN' else 0, ld['home_ownership']) 
+#I interpreted the question to indicate that only whether someone was a homeowner was of interest
+ld['cleanintrate'] = map(lambda x: str(x)[:-1], ld['int_rate'])
+ld['cleanintratenum'] = ld[['cleanintrate']].convert_objects(convert_numeric=True) #says is deprecated, but to_numeric doesn't work
+cleanintrate = map(lambda x: x/100, ld['cleanintratenum'])
+ld['int_rate'] = cleanintrate
+#cleaning interest rate
 
+def main():
 
+	X = ld[['annual_inc']]
+	y = ld['int_rate']
+	X = sm.add_constant(X)
+	est = sm.OLS(y, X).fit()
+	print(est.summary())
+	#modeling int_rate using income
 
-X = ld[['annual_inc' , 'hodummy']]
-y = ld['int_rate']
-X = sm.add_constant(X)
-est = sm.OLS(y, X).fit()
-#est1 = sm.OLS(formula='int_rate ~ annual_inc : home_ownership', data = ld).fit()
-print(est.summary())
-#print est1.params, '\n'
-#print est2.params
+	X1 = ld[['annual_inc', 'hodummy']]
+	y1 = ld['int_rate']
+	X1 = sm.add_constant(X1)
+	est1 = sm.OLS(y1, X1).fit()
+	print(est1.summary())
+	#modeling int_rate using income and homeownership
+
+if __name__ == "__main__":
+    main()
 
 
 
